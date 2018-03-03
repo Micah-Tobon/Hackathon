4fbfc91ae12429792aac452fdc7f85512862dafb
Djacques63 <37026533+Djacques63@users.noreply.github.com>
2018-03-03 15:47:18 -0500

    Add files via upload

---
+++ randomword.js
@@ -0,0 +1,111 @@
+function main(){
+  srand(time(NULL));
+  var wordNumber = rand();
+  wordNumber %= 34 +1;
+  switch(wordNumber){
+    case  1:
+      print  ("Artichoke");
+      break;
+    case  2:
+      print  ("Balloon");
+      break;
+    case  3:
+      print  ("Leather");
+      break;
+    case  4:
+      print  ("Kaleidoscope");
+      break;
+    case  5:
+      print  ("Bow");
+      break;
+    case  6:
+      print  ("Toes");
+      break;
+    case  7:
+      print  ("Hoes");
+      break;
+    case  8:
+      print  ("Hammer");
+      break;
+    case  9:
+      print  ("Guitar");
+      break;
+    case  10:
+      print  ("Boy");
+      break;
+    case  11:
+      print  ("Girl");
+      break;
+    case  12:
+      print  ("Film");
+      break;
+    case  13:
+      print  ("Explosive");
+      break;
+    case  14:
+      print  ("Kaleidoscope");
+      break;
+    case  15:
+      print  ("Explosive");
+      break;
+    case  16:
+      print  ("Onion");
+      break;
+    case  17:
+      print  ("Taco");
+      break;
+    case  18:
+      print  ("Mosquito");
+      break;
+    case  19:
+      print  ("Monster");
+      break;
+    case  20:
+      print  ("Neat");
+      break;
+    case  21:
+      print  ("Beat");
+      break;
+    case  22:
+      print  ("Meat");
+      break;
+    case  23:
+      print  ("Liquid");
+      break;
+    case  24:
+      print  ("Leg");
+      break;
+    case 25:
+      print  ("Leather");
+      break;
+    case 26:
+      print  ("Army");
+      break;
+    case 27:
+      print  ("Throw");
+      break;
+    case 28:
+      print  ("Grenade");
+      break;
+    case 29:
+      print  ("Bomb");
+      break;
+    case 30:
+      print  ("Desctruction");
+      break;
+    case 31:
+      print  ("Robot");
+      break;
+    case 32:
+      print  ("Rock");
+      break;
+    case 33:
+      print  ("Potato");
+      break;
+    case 34:
+      print  ("Barbecue");
+      break;
+
+  }
+  return 0;
+}
